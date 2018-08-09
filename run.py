0ca55d360947918722031745fceff016774e48eb
Dmitriy <dimadeck94@gmail.com>
2018-08-09 13:09:28 +0300

    Simple Twisted Chat v.0.1

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from asyncio_v1 import V1
+import twisted_simple_chat as tw_chat
 
 if __name__ == '__main__':
-    v1 = V1(5, 4)
-    v1.engine()
+    tw_chat.main()
--- twisted_simple_chat.py
+++ twisted_simple_chat.py
@@ -2,32 +2,77 @@ from twisted.internet import reactor, protocol
 from twisted.protocols.basic import LineReceiver
 
 
-class MyChatProtocol(LineReceiver):
+class Chat(LineReceiver):
+    def __init__(self, users):
+        self.users = users
+        self.name = None
+        self.register = False
+
     def connectionMade(self):
-        print("New connection")
-        self.factory.clients.append(self)
+        print("[DEBUG] - New connection")
+        self.send_current("What's your name?")
 
     def connectionLost(self, reason):
-        print("Lost connection")
-        self.factory.clients.remove(self)
+        print("[DEBUG] - Lost connection")
+        if self.name in self.users:
+            self.logging(f'[System] - {self.name} logout.')
+            del self.users[self.name]
 
     def lineReceived(self, line):
-        print(line)
-        for c in self.factory.clients:
-            if c is not self:
-                c.sendLine(line)
+        print(f'[DEBUG] - {line}')
+        try:
+            line = line.decode('utf-8')
+        except:
+            pass
+        if self.register:
+            self.chat_engine(line)
+        else:
+            self.register_engine(line)
 
-    def send_message(self, msg):
-        pass
+    def chat_engine(self, message):
+        if message.startswith('/'):
+            if message == '/logout':
+                self.stopProducing()  # temporary method?
+        else:
+            message = f'[{self.name}]: {message}'
+            self.send_all(message)
+
+    def register_engine(self, name):
+        if name in self.users:
+            self.send_current("[System] Name taken, please choose another.")
+            return
+        self.name = name
+        self.users[name] = self
+        self.register = True
+        self.send_current(f"[System] Welcome, {name}!")
+        self.logging(f'[System] - {name} login.')
+
+    def send_current(self, msg):
+        self.sendLine(bytes(msg, 'utf-8'))
+
+    def send_all(self, message):
+        for name, protocol in self.users.items():
+            protocol.sendLine(bytes(message, 'utf-8'))
+
+    def logging(self, message):
+        print(message)
+        self.send_all(message)
 
 
-class MyChatFactory(protocol.ServerFactory):
-    protocol = MyChatProtocol
+class Factory(protocol.ServerFactory):
 
     def startFactory(self):
-        self.clients = []
+        self.users = {}
+
+    def buildProtocol(self, addr):
+        return Chat(self.users)
+
+
+def main(port=1234):
+    print(f'[DEBUG] - Twisted Server start on {port}')
+    reactor.listenTCP(port, Factory())
+    reactor.run()
 
 
 if __name__ == '__main__':
-    reactor.listenTCP(1234, MyChatFactory())
-    reactor.run()
+    main()
