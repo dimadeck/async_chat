1d7e1011308aa51891911a21b9a1ac7a0b5fb329
Dmitriy <dimadeck94@gmail.com>
2018-08-14 11:55:26 +0300

    rename Log to ColorServer and move to color module

--- base_server/base_server.py
+++ base_server/base_server.py
@@ -1,30 +1,28 @@
-from base_server.color_module import ColorModule
+from base_server.color_module import Color, ColorChat, ColorServer
 from base_server.connected import Connected
 from base_server.data_parser import DataParser
-from base_server.logger import Log
 
 
 class ChatKernel:
     def __init__(self, connections=None, parse_strip='\r\n'):
         self.connections = connections if connections is not None else Connected()
         self.parse_strip = parse_strip
-        self.logger = Log()
 
     def engine(self, request, writer, addr):
         if len(request) > 1:
             req_dict = DataParser(request, strip=self.parse_strip)
-            self.logger.log_engine(mode='request', data_list=req_dict.data_list)
+            ColorServer.log_engine(mode='request', data_list=req_dict.data_list)
 
             if not self.connections.is_exist_connection(writer):
                 self.connections.add_connection(writer)
-                self.logger.log_engine(mode='new', addr=addr)
+                ColorServer.log_engine(mode='new', addr=addr)
 
             if req_dict.status == 0:
                 if self.run_command(req_dict, writer) == -1:
                     return -1
             else:
                 message = req_dict.STATUS_DICT[req_dict.status]
-                message = ColorModule.add_suffix('error', message)
+                message = ColorChat.add_suffix('error', message)
                 self.send_message(writer, message)
         if not request:
             self.logout(writer)
@@ -35,7 +33,7 @@ class ChatKernel:
         cmd = req_dict.cmd
         param = req_dict.parameter
         body = req_dict.body
-        self.logger.log_engine(mode='parse', cmd=cmd, param=param, body=body)
+        ColorServer.log_engine(mode='parse', cmd=cmd, param=param, body=body)
 
         if self.connections.is_register(connection):
             if cmd == 'msg' or cmd == 'msgall':
@@ -44,19 +42,19 @@ class ChatKernel:
                 self.logout(connection)
                 return -1
             elif cmd == 'debug':
-                self.logger.log_engine(mess=self.connections.connections)
-                self.logger.log_engine(mess=self.connections.users)
+                ColorServer.log_engine(mess=self.connections.connections)
+                ColorServer.log_engine(mess=self.connections.users)
             else:
                 message = None
                 if cmd == 'whoami':
                     message = self.connections.get_name(connection)
-                    message = ColorModule.add_suffix('info', message)
+                    message = ColorChat.add_suffix('info', message)
                 elif cmd == 'userlist':
                     message = self.connections.get_user_list()
-                    message = ColorModule.add_suffix('info', message)
+                    message = ColorChat.add_suffix('info', message)
                 elif cmd == 'login':
                     message = 'Already login!'
-                    message = ColorModule.add_suffix('error', message)
+                    message = ColorChat.add_suffix('error', message)
                 if message is not None:
                     self.send_message(connection, message)
         else:
@@ -64,7 +62,7 @@ class ChatKernel:
                 self.login(connection, param)
             else:
                 message = 'First login!'
-                message = ColorModule.add_suffix('error', message)
+                message = ColorChat.add_suffix('error', message)
                 self.send_message(connection, message)
         return 0
 
@@ -72,8 +70,8 @@ class ChatKernel:
         sender = self.connections.get_name(connection)
         message = ' '.join(body)
 
-        sender = ColorModule.color_user(sender)
-        message = ColorModule.color_message(message)
+        sender = ColorChat.color_user(sender)
+        message = ColorChat.color_message(message)
 
         if cmd == 'msg':
             username = param
@@ -84,8 +82,8 @@ class ChatKernel:
                 self.send_message(connection, message)
             else:
                 message = 'not found!'
-                username = ColorModule.color_user(username)
-                message = ColorModule.add_suffix('error', f'[{username}]: {message}')
+                username = ColorChat.color_user(username)
+                message = ColorChat.add_suffix('error', f'[{username}]: {message}')
                 self.send_message(connection, message)
         elif cmd == 'msgall':
             message = f'[{sender}]: {message}'
@@ -106,34 +104,37 @@ class ChatKernel:
     def login(self, connection, username):
 
         if self.connections.register_user(connection, username) == 0:
-            username = ColorModule.color_user(username)
 
+            ColorServer.log_engine(mode='login', username=username)
             message = "login to chat."
-            message = ColorModule.change_color('green', message)
+            message = Color.change_color('green', message)
+
+            username = ColorChat.color_user(username)
 
             message = f'[{username}] {message}'
-            message = ColorModule.add_suffix('sys', message)
+            message = ColorChat.add_suffix('sys', message)
 
             self.send_all(message)
-            self.logger.log_engine(mode='login', username=username)
+
         else:
-            username = ColorModule.color_user(username)
+            username = ColorChat.color_user(username)
             message = 'already exist!'
             message = f'[{username}]: {message}'
-            message = ColorModule.add_suffix('error', message)
+            message = ColorChat.add_suffix('error', message)
 
             self.send_message(connection, message)
 
     def logout(self, connection):
         username = self.connections.get_name(connection)
 
-        message = "logout from chat."
-        message = ColorModule.change_color('red', message)
-        username = ColorModule.color_user(username)
+        ColorServer.log_engine(mode='logout', username=username)
 
+        message = "logout from chat."
+        message = Color.change_color('red', message)
+        username = ColorChat.color_user(username)
+        message = f'[{username}] {message}'
+        message = ColorChat.add_suffix('sys', message)
         self.close_connection(connection)
         self.connections.drop_connection(connection)
-        message = f'[{username}] {message}'
-        message = ColorModule.add_suffix('sys', message)
+
         self.send_all(message)
-        self.logger.log_engine(mode='logout', username=username)
--- base_server/color_module.py
+++ base_server/color_module.py
@@ -1,17 +1,31 @@
 from clint.textui import colored
 
+SERVER_INFO = True
+DEBUG_MODE = False
 
-class ColorModule:
+
+class Color:
+    @staticmethod
+    def change_color(color, message):
+        color_set = {'red': colored.red, 'green': colored.green, 'white': colored.white, 'yellow': colored.yellow,
+                     'blue': colored.blue, 'black': colored.black}
+        try:
+            return color_set[color](message)
+        except:
+            return message
+
+
+class ColorChat:
     @staticmethod
     def color_message(message):
-        return ColorModule.change_color('white', message)
+        return Color.change_color('white', message)
 
     @staticmethod
     def add_suffix(mode, message):
         color_suffix = {
-            'error': ColorModule.change_color('red', "[Error]: "),
-            'sys': ColorModule.change_color('green', "[System Message]: "),
-            'info': ColorModule.change_color('blue', "[INFO]: ")}
+            'error': Color.change_color('red', "[Error]: "),
+            'sys': Color.change_color('green', "[System Message]: "),
+            'info': Color.change_color('blue', "[INFO]: ")}
         try:
             suffix = color_suffix[mode]
             message = f'{suffix}{message}'
@@ -21,13 +35,55 @@ class ColorModule:
 
     @staticmethod
     def color_user(username):
-        return ColorModule.change_color('yellow', username)
+        return Color.change_color('yellow', username)
+
+
+class ColorServer:
+    @staticmethod
+    def log_engine(mode=None, mess=None, **kw):
+        message = None
+        color = None
+        suffix = None
+        if mess is not None:
+            print(Color.change_color('white', mess))
+
+        if mode is not None:
+            if SERVER_INFO:
+                suffix = '[SERVER INFO] - '
+                message, color = ColorServer.server_info(mode, **kw)
+            if DEBUG_MODE:
+                suffix = '[DEBUG] - '
+                color = 'blue'
+                message = ColorServer.debug_mode(mode, **kw)
+
+            if message is not None:
+                if suffix is not None:
+                    message = f'{suffix}{message}'
+                if color is not None:
+                    message = Color.change_color(color, message)
+
+                print(message)
 
     @staticmethod
-    def change_color(color, message):
-        color_set = {'red': colored.red, 'green': colored.green, 'white': colored.white, 'yellow': colored.yellow,
-                     'blue': colored.blue, 'black': colored.black}
-        try:
-            return color_set[color](message)
-        except:
-            return message
+    def server_info(mode, **kw):
+        message = None
+        color = None
+        if mode == 'login':
+            message = f'[{kw["username"]}] login to chat.'
+            color = 'green'
+        elif mode == 'logout':
+            message = f'[{kw["username"]}] logout from chat.'
+            color = 'red'
+        elif mode == 'new':
+            message = f'New connection: {kw["addr"]}'
+            color = 'yellow'
+        return message, color
+
+    @staticmethod
+    def debug_mode(mode, **kw):
+        message = None
+        if mode == 'request':
+            message = f'Request: {kw["data_list"]}'
+        elif mode == 'parse':
+            message = f'Command: {kw["cmd"]}\nParameter: {kw["param"]}\nBody: {kw["body"]}'
+        return message
