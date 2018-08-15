ed2d76755ff5f9c5013507cab695cb389f91f31a
Dmitriy <dimadeck94@gmail.com>
2018-08-15 13:34:50 +0300

    split aiohttp_views code to many definition

--- aiohttp_views.py
+++ aiohttp_views.py
@@ -10,32 +10,45 @@ async def index(request):
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
         return aiohttp_jinja2.render_template('index.html', request, {})
-
     await ws_current.prepare(request)
-
-    name = await ws_current.receive()
-    name = name.data
-
+    name = await get_name(ws_current)
     print(f'{name} joined.')
+    await send_to(ws_current, action='connect', name=name)
+    await send_all(request, action='join', name=name)
+    add_connection(request, ws_current, name)
+    await chat_engine(request, ws_current, name)
+    await close_connection(request, name)
+    print(f'{name} disconnected.')
+    return ws_current
 
-    await ws_current.send_json({'action': 'connect', 'name': name})
-    await ws_current.send_json({'action': 'get_version', 'version': VERSION})
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'join', 'name': name})
-    request.app['websockets'][name] = ws_current
 
+async def send_all(request, **kwargs):
+    for connection in request.app['websockets'].values():
+        await connection.send_json(kwargs)
+
+
+async def send_to(connection, **kwargs):
+    await connection.send_json(kwargs)
+
+
+async def get_name(connection):
+    name = await connection.receive()
+    return name.data
+
+
+def add_connection(request, connection, name):
+    request.app['websockets'][name] = connection
+
+
+async def chat_engine(request, connection, name):
     while True:
-        msg = await ws_current.receive()
+        msg = await connection.receive()
         if msg.type == aiohttp.WSMsgType.text:
-            for ws in request.app['websockets'].values():
-                await ws.send_json(
-                    {'action': 'sent', 'name': name, 'text': msg.data})
+            await send_all(request, action='sent', name=name, text=msg.data)
         else:
             break
 
-    del request.app['websockets'][name]
-    print(f'{name} disconnected.')
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'disconnect', 'name': name})
 
-    return ws_current
+async def close_connection(request, name):
+    del request.app['websockets'][name]
+    await send_all(request, action='disconnect', name=name)
