8d09f903d29e55d4fb72d256408e0500d6d67309
Dmitriy <dimadeck94@gmail.com>
2018-08-15 17:37:46 +0300

    catch Runtime exp

--- aiohttp_web.py
+++ aiohttp_web.py
@@ -9,25 +9,29 @@ from base_server.connected import Connected
 
 class AioChat(ChatKernel):
     def __init__(self, connections):
-        super(AioChat, self).__init__()
+        super(AioChat, self).__init__(connections=connections)
         self.app = web.Application()
-        self.app['websockets'] = connections
+        self.app['websockets'] = self.connections
 
-    async def init_app(self):
+    def init_app(self):
         self.app.on_shutdown.append(self.shutdown)
-        aiohttp_jinja2.setup(
-            self.app, loader=jinja2.PackageLoader('view', 'templates'))
+        aiohttp_jinja2.setup(self.app, loader=jinja2.PackageLoader('view', 'templates'))
         self.app.router.add_get('/', index)
         return self.app
 
     async def shutdown(self, app):
-        for ws in self.app['websockets'].users.keys():
-            await ws.close()
+        try:
+            for ws in self.app['websockets'].users.keys():
+                await ws.close()
+        except RuntimeError:
+            pass
         app['websockets'].clear_all()
 
 
-def main():
-    server = AioChat(Connected())
+def main(connections=None):
+    if connections is None:
+        connections = Connected()
+    server = AioChat(connections)
     app = server.init_app()
     web.run_app(app)
 
--- bind_aio_as.py
+++ bind_aio_as.py
@@ -1,5 +1,12 @@
+from aiohttp_web import main as aio_main
+from chat_asyncio import main as as_main
+from base_server.connected import Connected
+
+
 def main():
-    pass
+    connections = Connected()
+    as_main(connections)
+    aio_main(connections)
 
 
 if __name__ == '__main__':
