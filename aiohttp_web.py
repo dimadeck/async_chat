8bd1bf98ab136ca1b025df52caf0905e006d37fc
Dmitriy <dimadeck94@gmail.com>
2018-08-16 16:14:54 +0300

    move send_all method to aioChat

--- aiohttp_web.py
+++ aiohttp_web.py
@@ -13,7 +13,6 @@ class AioChat(ChatKernel):
     def __init__(self, connections):
         super(AioChat, self).__init__(connections=connections)
         self.app = web.Application()
-        self.app['websockets'] = self.connections
 
     def init_app(self):
         self.app.on_shutdown.append(self.shutdown)
@@ -23,13 +22,11 @@ class AioChat(ChatKernel):
 
     async def shutdown(self, app):
         try:
-            # for ws in self.connections.users.keys():
-            for ws in self.app['websockets'].users.keys():
+            for ws in self.get_users():
                 await ws.close()
         except RuntimeError:
             pass
-        # self.connections.clear_all()
-        app['websockets'].clear_all()
+        self.clear_connections()
 
     ##
 
@@ -40,58 +37,43 @@ class AioChat(ChatKernel):
             return aiohttp_jinja2.render_template('index.html', request, {'version': VERSION})
         await ws_current.prepare(request)
         name = await self.get_name(ws_current)
-        await self.send_mess(ws_current, action='connect', name=name)
 
-        request.app['websockets'].add_connection(ws_current)
-        request.app['websockets'].register_user(ws_current, name)
+        mes = {'action': 'connect', 'name': name}
+        await self.send_message(ws_current, mes)
 
-        # self.connections.add_connection(ws_current)
+        self.add_connection(ws_current)
         self.login(ws_current, name)
 
         mes = {'action': 'join', 'name': name}
-        await self.send_al(request, action='join', name=name)
-        # await self.send_all(mes)
-        # await self.chat_engine(ws_current, name)
-        await self.chat_engine(request, ws_current, name)
-        await self.close_connect(request, ws_current, name)
+        await self.send_all(mes)
+        await self.chat_engine(ws_current, name)
+        await self.close_connect(ws_current, name)
         return ws_current
 
-    async def send_message(self, user, kwargs):
-        await user.send_json(kwargs)
+    async def send_all(self, message):
+        for user in self.get_users():
+            await self.send_message(user, message)
 
-    async def send_mess(self, user, **kwargs):
-        await user.send_json(kwargs)
-
-    async def send_al(self, request, **kwargs):
-        # for user in self.connections.users.keys():
-        for user in request.app['websockets'].users.keys():
-            await self.send_mess(user, **kwargs)
+    async def send_message(self, user, message):
+        await user.send_json(message)
 
     async def get_name(self, connection):
         name = await connection.receive()
         return name.data
 
-    async def chat_engine(self, request, connection, name):
-        # async def chat_engine(self, connection, name):
-
+    async def chat_engine(self, connection, name):
         while True:
             msg = await connection.receive()
             if msg.type == aiohttp.WSMsgType.text:
-                # mes = {'action': 'sent', 'name': name, 'text': msg.data}
-                # await self.send_all(mes)
-
                 mes = {'action': 'sent', 'name': name, 'text': msg.data}
-                await self.send_al(request, action='sent', name=name, text=msg.data)
+                await self.send_all(mes)
             else:
                 break
 
-    async def close_connect(self,request, connection, name):
-#    async def close_connect(self,connection, name):
-
+    async def close_connect(self, connection, name):
         self.connections.drop_connection(connection)
         mes = {'action': 'disconnect', 'name': name}
-        await self.send_al(request, action='disconnect', name=name)
-        # await self.send_all(mes)
+        await self.send_all(mes)
 
 
 ##
