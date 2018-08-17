077d672e13871cf6e4b1555852ea815b46bed93a
Dmitriy <dimadeck94@gmail.com>
2018-08-17 12:25:22 +0300

    optimize launch

--- bind_aio_as.py
+++ bind_aio_as.py
@@ -7,33 +7,51 @@ from chat_tornado import main as tor_main
 from chat_twisted import main as tw_main
 
 
-class A:
-    def __init__(self, func, port, connections):
-        self.func = func
+class Connect:
+    def __init__(self, server, port, connections):
+        self.server = server
         self.connections = connections
         self.port = port
 
     def __call__(self, sleep_time=0.5):
-        self.func(port=self.port, connections=self.connections)
+        self.server(port=self.port, connections=self.connections)
         sleep(sleep_time)
 
 
+class LaunchProcesses:
+    def __init__(self, servers, connections, ports):
+        self.processes = []
+        self.ports = ports
+        self.connections = connections
+        self.servers = servers
+
+    def engine(self):
+        self.create_process()
+        self.start_processes()
+        self.join_processes()
+
+    def create_process(self):
+        for server in self.servers:
+            port = self.get_port()
+            target = Connect(server, port, connections)
+            self.processes.append(Process(target=target))
+
+    def get_port(self):
+        return self.ports.pop()
+
+    def start_processes(self):
+        for process in self.processes:
+            process.start()
+
+    def join_processes(self):
+        for process in self.processes:
+            process.join()
+
+
 if __name__ == '__main__':
+    servers = [as_main, tor_main, tw_main]
     connections = Connected()
     ports = [8000, 8080, 8888]
 
-    a = A(as_main, ports.pop(), connections)
-    b = A(tor_main, ports.pop(), connections)
-    c = A(tw_main, ports.pop(), connections)
-
-    p1 = Process(target=a)
-    p2 = Process(target=b)
-    p3 = Process(target=c)
-
-    p1.start()
-    p2.start()
-    p3.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
+    launch = LaunchProcesses(servers=servers, connections=connections, ports=ports)
+    launch.engine()
