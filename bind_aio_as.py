b497b12fd5aa7c9050bd7cee7e6b71d96cd3f352
Dmitriy <dimadeck94@gmail.com>
2018-08-17 11:39:17 +0300

    bad try2 bind tcp

--- bind_aio_as.py
+++ bind_aio_as.py
@@ -1,11 +1,10 @@
 from multiprocessing import Process
 from time import sleep
-
-from aiohttp_web import main as aio_main
-from base_server.connected import Connected
 from chat_asyncio import main as as_main
 from chat_tornado import main as tor_main
+from chat_twisted import main as tw_main
 
+from base_server.connected import Connected
 
 
 class A:
@@ -20,14 +19,18 @@ class A:
 
 if __name__ == '__main__':
     connections = Connected()
-
     a = A(as_main, connections)
     b = A(tor_main, connections)
+    c = A(tw_main, connections)
 
     p1 = Process(target=a)
     p2 = Process(target=b)
+    p3 = Process(target=c)
+
     p1.start()
     p2.start()
+    p3.start()
 
     p1.join()
     p2.join()
+    p3.join()
