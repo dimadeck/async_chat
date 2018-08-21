dbd57d1c55f1404651289a07e56c6252d441c786
Dmitriy Konovalov <dimadeck94@gmail.com>
2018-08-21 19:27:31 +0300

    duplicate kernel

--- kernel/chat_kernel.py
+++ kernel/chat_kernel.py
@@ -1,17 +1,64 @@
-from base_server.base_server import ChatKernel
+from base_server.connected import Connected
 from base_server.tcp_server.data_parser import DataParser
 from kernel.chat_pack_message import PackMessage
 from kernel.chat_protocol import ChatProtocol
 
 
-class TCPKernel(ChatKernel):
+class ChatKernel:
     def __init__(self, connections=None, parse_strip='\r\n', method_send_message=None, method_close_connection=None):
-        super(TCPKernel, self).__init__(connections=connections)
+        self.connections = self.init_connection_list(connections)
         self.parse_strip = parse_strip
         if method_send_message is not None:
             self.send_message = method_send_message
         if method_close_connection is not None:
             self.close_connection = method_close_connection
+
+    @staticmethod
+    def init_connection_list(connections):
+        return connections if connections is not None else Connected()
+
+    @staticmethod
+    def send_message(connection, message):
+        raise NotImplementedError
+
+    @staticmethod
+    def close_connection(connection):
+        raise NotImplementedError
+
+    def send_all(self, message):
+        for user in self.get_users():
+            self.send_message(user, message)
+
+    def login(self, connection, username):
+        return self.connections.register_user(connection, username)
+
+    def logout(self, connection):
+        self.close_connection(connection)
+        self.connections.drop_connection(connection)
+
+    def add_connection(self, connection):
+        return self.connections.add_connection(connection)
+
+    def is_register(self, connection):
+        return self.connections.is_register(connection)
+
+    def get_connections(self):
+        return self.connections.connections
+
+    def clear_connections(self):
+        self.connections.clear_all()
+
+    def get_users(self):
+        return self.connections.users
+
+    def get_name_by_connection(self, connection):
+        return self.connections.get_name(connection)
+
+    def get_connection_by_name(self, username):
+        return self.connections.get_connection(username)
+
+    def get_username_list(self):
+        return self.connections.get_username_list()
 
     def engine(self, request, writer, addr):
         if len(request) > 1:
@@ -35,17 +82,18 @@ class TCPKernel(ChatKernel):
         message = ' '.join(req_dict.body) if body is not None else None
 
         if self.is_register(connection):
-            methods = {'login': (self.NEW_alredy_login, {'connection': connection}),
+            methods = {'login': (self.error_alredy_login, {'connection': connection}),
                        'logout': (self.logout_engine, {'connection': connection}),
-                       'msg': (self.NEW_send_mess, {'connection': connection, 'username': param, 'message': message}),
-                       'msgall': (self.NEW_send_all, {'connection': connection, 'message': message}),
-                       'debug': (self.NEW_debug, {}),
-                       'whoami': (self.NEW_whoami, {'connection': connection}),
-                       'userlist': (self.NEW_userlist, {'connection': connection})
+                       'msg': (
+                       self.send_message_engine, {'connection': connection, 'username': param, 'message': message}),
+                       'msgall': (self.send_all_engine, {'connection': connection, 'message': message}),
+                       'debug': (self.debug_engine, {}),
+                       'whoami': (self.whoami_engine, {'connection': connection}),
+                       'userlist': (self.userlist_engine, {'connection': connection})
                        }
         else:
-            methods = {'login': (self.NEW_login, {'connection': connection, 'username': param}),
-                       'empty': (self.NEW_first_login, {'connection': connection})}
+            methods = {'login': (self.login_engine, {'connection': connection, 'username': param}),
+                       'empty': (self.error_first_login, {'connection': connection})}
         protocol = ChatProtocol(**methods)
         return protocol.engine(cmd)
 
@@ -57,7 +105,7 @@ class TCPKernel(ChatKernel):
             self.send_all(message)
             return -1
 
-    def NEW_login(self, connection, username):
+    def login_engine(self, connection, username):
         if self.login(connection, username) == 0:
             message = PackMessage.system_message('login', username=username)
             self.send_all(message)
@@ -65,15 +113,15 @@ class TCPKernel(ChatKernel):
             message = PackMessage.system_error('user_exist')
             self.send_message(connection, message)
 
-    def NEW_alredy_login(self, connection):
+    def error_alredy_login(self, connection):
         message = PackMessage.system_error('already_login')
         self.send_message(connection, message)
 
-    def NEW_first_login(self, connection):
+    def error_first_login(self, connection):
         message = PackMessage.system_error('first_login')
         self.send_message(connection, message)
 
-    def NEW_send_mess(self, connection, username, message):
+    def send_message_engine(self, connection, username, message):
         sender = self.get_name_by_connection(connection)
         user = self.get_connection_by_name(username)
         if user is not None:
@@ -84,24 +132,24 @@ class TCPKernel(ChatKernel):
             message = PackMessage.system_error('not_found', username=username)
             self.send_message(connection, message)
 
-    def NEW_send_all(self, connection, message):
+    def send_all_engine(self, connection, message):
         sender = self.get_name_by_connection(connection)
         message = PackMessage.chat_message(username=sender, message=message)
         self.send_all(message)
 
-    def NEW_debug(self):
+    def debug_engine(self):
         connections = self.get_connections()
         userlist = self.get_users()
 
         print(PackMessage.message(connections))
         print(PackMessage.message(userlist))
 
-    def NEW_whoami(self, connection):
+    def whoami_engine(self, connection):
         username = self.get_name_by_connection(connection)
         message = PackMessage.message(username)
         self.send_message(connection, message)
 
-    def NEW_userlist(self, connection):
+    def userlist_engine(self, connection):
         userlist = ', '.join(self.get_username_list())
         message = PackMessage.message(userlist)
         self.send_message(connection, message)
