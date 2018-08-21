505be2a738b9b34f108d303ca02d5fbc342b0654
Dmitriy <dimadeck94@gmail.com>
2018-08-21 15:52:50 +0300

    change to new PackMessage

--- base_server/tcp_server/tcp_kernel.py
+++ base_server/tcp_server/tcp_kernel.py
@@ -1,8 +1,6 @@
 from base_server.base_server import ChatKernel
-# from base_server.tcp_server.color_module import ColorServer
 from base_server.tcp_server.data_parser import DataParser
 from chat_pack_message import PackMessage
-# from base_server.tcp_server.pack_message import PackMessage
 from chat_protocol import ChatProtocol
 
 
@@ -14,16 +12,12 @@ class TCPKernel(ChatKernel):
     def engine(self, request, writer, addr):
         if len(request) > 1:
             if self.add_connection(writer) == 0:
-                PackMessage.engine('server', 'new', addr=addr)
-                # ColorServer.log_engine(mode='new', addr=addr)
-
+                print(PackMessage.server_message('new', addr=addr))
             req_dict = DataParser(request, strip=self.parse_strip)
-            ColorServer.log_engine(mode='request', data_list=req_dict.data_list)
-
             if req_dict.status == 0:
                 return self.run_command(req_dict, writer)
             else:
-                message = PackMessage.send_response_for_bad_request(req_dict)
+                message = PackMessage.system_error('bad_request', message=req_dict.STATUS_DICT[req_dict.status])
                 self.send_message(writer, message)
         elif not request:
             self.logout_engine(writer)
@@ -34,13 +28,12 @@ class TCPKernel(ChatKernel):
         cmd = req_dict.cmd
         param = req_dict.parameter
         body = req_dict.body
-        ColorServer.log_engine(mode='parse', cmd=cmd, param=param, body=body)
 
         if self.is_register(connection):
             methods = {'login': (self.NEW_alredy_login, {'connection': connection}),
                        'logout': (self.logout_engine, {'connection': connection}),
-                       'msg': (self.NEW_send_mess, {'connection': connection, 'username': param, 'body': body}),
-                       'msgall': (self.NEW_send_all, {'connection': connection, 'body': body}),
+                       'msg': (self.NEW_send_mess, {'connection': connection, 'username': param, 'message': body}),
+                       'msgall': (self.NEW_send_all, {'connection': connection, 'message': body}),
                        'debug': (self.NEW_debug, {}),
                        'whoami': (self.NEW_whoami, {'connection': connection}),
                        'userlist': (self.NEW_userlist, {'connection': connection})
@@ -49,57 +42,61 @@ class TCPKernel(ChatKernel):
             methods = {'login': (self.NEW_login, {'connection': connection, 'username': param}),
                        'empty': (self.NEW_first_login, {'connection': connection})}
         protocol = ChatProtocol(**methods)
-        return protocol.engine(req_dict.cmd)
+        return protocol.engine(cmd)
 
     def logout_engine(self, connection):
         username = self.get_name_by_connection(connection)
         self.logout(connection)
-        message = PackMessage.send_logout(username)
+        message = PackMessage.system_message('logout', username=username)
         self.send_all(message)
         return -1
 
     def NEW_login(self, connection, username):
         if self.login(connection, username) == 0:
-            message = PackMessage.send_success_login(username)
-            self.send_all(message)
+            message = PackMessage.system_message('login', username=username)
         else:
-            # user exist
-            pass
+            message = PackMessage.system_error('user_exist')
+        self.send_all(message)
 
     def NEW_alredy_login(self, connection):
-        message = 'Already login!'
+        message = PackMessage.system_error('already_login')
         self.send_message(connection, message)
 
     def NEW_first_login(self, connection):
-        message = PackMessage.send_first_login()
+        message = PackMessage.system_error('first_login')
         self.send_message(connection, message)
 
-    def NEW_send_mess(self, connection, username, body):
+    def NEW_send_mess(self, connection, username, message):
         sender = self.get_name_by_connection(connection)
         user = self.get_connection_by_name(username)
+        message = ' '.join(message)
         if user is not None:
-            message = PackMessage.text_message(is_exist=True, sender=sender, body=body)
+            message = PackMessage.chat_message(username=sender, message=message, private=True)
             self.send_message(user, message)
             self.send_message(connection, message)
         else:
-            message = PackMessage.text_message(is_exist=False, username=username)
+            message = PackMessage.system_error('not_found', username=username)
             self.send_message(connection, message)
 
-    def NEW_send_all(self, connection, body):
+    def NEW_send_all(self, connection, message):
         sender = self.get_name_by_connection(connection)
-        message = PackMessage.text_message(is_exist=True, private=False, sender=sender, body=body)
+        message = ' '.join(message)
+        message = PackMessage.chat_message(username=sender, message=message)
         self.send_all(message)
 
     def NEW_debug(self):
-        ColorServer.log_engine(mess=self.get_connections())
-        ColorServer.log_engine(mess=self.get_users())
+        connections = ' '.join(self.get_connections())
+        userlist = ' '.join(self.get_users())
+
+        print(PackMessage.message(connections))
+        print(PackMessage.message(userlist))
 
     def NEW_whoami(self, connection):
         username = self.get_name_by_connection(connection)
-        message = PackMessage.get_info(cmd='whoami', message=username)
+        message = PackMessage.message(username)
         self.send_message(connection, message)
 
     def NEW_userlist(self, connection):
-        userlist = self.get_username_list()
-        message = PackMessage.get_info(cmd='userlist', message=userlist)
+        userlist = ', '.join(self.get_username_list())
+        message = PackMessage.message(userlist)
         self.send_message(connection, message)
