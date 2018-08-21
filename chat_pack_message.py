656fdd530c8b4ccf74efcb339377c5505bd3294c
Dmitriy <dimadeck94@gmail.com>
2018-08-21 15:53:06 +0300

    refactor pack_message - delete engine

--- chat_pack_message.py
+++ chat_pack_message.py
@@ -8,58 +8,47 @@ class PackMessage:
     MESSAGE_NEW_CONNECTION = 'New connection:'
     MESSAGE_LOGIN = 'login to chat.'
     MESSAGE_LOGOUT = 'logout from chat.'
-
     MESSAGE_FIRST_LOGIN = 'First login!'
     MESSAGE_ALREADY_LOGIN = 'Already login!'
-
     MESSAGE_USER_EXIST = 'Username already taken!'
     MESSAGE_NOT_FOUND = 'not found!'
 
     @staticmethod
-    def engine(engine_mode, mode, **kwargs):
-        engine_messaging = {
-            'server': {'start': (PackMessage.server_start, {kwargs['version'], kwargs['port']}),
-                       'new': (PackMessage.server_new_connection, {kwargs['addr']}),
-                       'login': (PackMessage.login, {kwargs['username']}),
-                       'logout': (PackMessage.logout, {kwargs['username']}),
-                       'debug': (PackMessage.message, {kwargs['message']})},
-            'system': {'login': (PackMessage.login, {kwargs['username']}),
-                       'logout': (PackMessage.logout, {kwargs['username']}),
-                       'error': (PackMessage.system_error, {kwargs['error_mode'], kwargs['message']}),
-                       'info': (PackMessage.message, {kwargs['message']})
-                       },
-            'chat': {'message': (PackMessage.chat_message, {kwargs['username'], kwargs['message']})}
-        }
+    def server_message(server_mode, version=None, port=None, addr=None, username=None, message=None):
+        if server_mode == 'start':
+            message = PackMessage.server_start(version, port)
+        elif server_mode == 'new':
+            message = PackMessage.server_new_connection(addr)
+        elif server_mode == 'login':
+            message = PackMessage.login(username)
+        elif server_mode == 'logout':
+            message = PackMessage.logout(username)
 
-        suffix = PackMessage.suffix(engine_mode, mode)
-        func = engine_messaging[engine_mode][mode][0]
-        args = engine_messaging[engine_mode][mode][1]
-        mess = func(args)
-        return PackMessage.add_suffix(suffix, mess)
+        return PackMessage.add_suffix(PackMessage.SERVER_SUFFIX, message)
 
     @staticmethod
-    def system_error(error_mode, **kwargs):
-        error_messaging = {'bad_request': kwargs['message'],
+    def system_message(system_mode, username=None, message=None):
+        if system_mode == 'login':
+            message = PackMessage.login(username)
+        elif system_mode == 'logout':
+            message = PackMessage.logout(username)
+
+        return PackMessage.add_suffix(PackMessage.SYSTEM_SUFFIX, message)
+
+    @staticmethod
+    def chat_message(username, message, private=False):
+        private_sym = '*' if private else ''
+        mess = f'[{username}{private_sym}]: {message}'
+        return mess
+
+    @staticmethod
+    def system_error(error_mode, message=None, username=None):
+        error_messaging = {'bad_request': message,
                            'first_login': PackMessage.MESSAGE_FIRST_LOGIN,
                            'already_login': PackMessage.MESSAGE_ALREADY_LOGIN,
                            'user_exist': PackMessage.MESSAGE_USER_EXIST,
-                           'not_found': PackMessage.MESSAGE_NOT_FOUND}
-        return error_messaging[error_mode]
-
-    @staticmethod
-    def suffix(m1, m2):
-        if m1 == 'chat':
-            suffix = None
-        elif m1 == 'server':
-            suffix = PackMessage.SERVER_SUFFIX
-        else:
-            if m2 == ('login' or 'logout'):
-                suffix = PackMessage.SYSTEM_SUFFIX
-            elif m2 == 'error':
-                suffix = PackMessage.ERROR_SUFFIX
-            else:
-                suffix = PackMessage.INFO_SUFFIX
-        return suffix
+                           'not_found': f'[{username}] {PackMessage.MESSAGE_NOT_FOUND}'}
+        return PackMessage.add_suffix(PackMessage.ERROR_SUFFIX, error_messaging[error_mode])
 
     @staticmethod
     def add_suffix(suffix, message):
@@ -84,12 +73,6 @@ class PackMessage:
     @staticmethod
     def logout(username):
         mess = f'[{username}] {PackMessage.MESSAGE_LOGOUT}'
-        return mess
-
-    @staticmethod
-    def chat_message(username, message, private=False):
-        private_sym = '*' if private else ''
-        mess = f'[{username}{private_sym}]: {message}'
         return mess
 
     @staticmethod
