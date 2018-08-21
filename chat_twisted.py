fee51c2e827f7481ae06c0e944e903dbcc07e8ac
Dmitriy Konovalov <dimadeck94@gmail.com>
2018-08-21 20:46:10 +0300

    refactor twisted_chat with New_chat_kernel

--- chat_twisted.py
+++ chat_twisted.py
@@ -1,12 +1,14 @@
 from twisted.internet import reactor, protocol
 from twisted.protocols.basic import LineReceiver
 
-from base_server.tcp_server.tcp_kernel import TCPKernel
+from kernel.chat_kernel import ChatKernel
+from kernel.chat_pack_message import PackMessage
 
 
-class Chat(LineReceiver, TCPKernel):
+class Chat(LineReceiver, ChatKernel):
     def __init__(self, connections, addr):
-        super(Chat, self).__init__(connections=connections, parse_strip='')
+        super(Chat, self).__init__(connections=connections, parse_strip='', method_send_message=self.send_message,
+                                   method_close_connection=self.close_connection)
         self.addr = addr
 
     def lineReceived(self, line):
@@ -23,15 +25,18 @@ class Chat(LineReceiver, TCPKernel):
 
 class Factory(protocol.ServerFactory):
     def __init__(self, connections):
+        self.connections = ChatKernel.init_connection_list(connections)
         super(Factory, self).__init__()
-        self.connections = TCPKernel.init_connection_list(connections)
 
     def buildProtocol(self, addr):
         return Chat(self.connections, addr)
 
 
+VERSION = 'Twisted_Chat'
+
+
 def main(port=1234, connections=None):
-    print(f'[SERVER INFO] - Twisted server started on {port} port.')
+    print(PackMessage.server_message('start', version=VERSION, port=port))
     reactor.listenTCP(port, Factory(connections=connections))
     reactor.run()
 
