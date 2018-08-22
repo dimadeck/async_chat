1c796090cf958a577a06d11cd5d9e38f578806b4
Dmitriy <dimadeck94@gmail.com>
2018-08-22 14:45:52 +0300

    alternative way for tw_ws

--- web_twisted.py
+++ web_twisted.py
@@ -5,15 +5,14 @@ from twisted.internet import reactor
 # var ws = new WebSocket("ws://127.0.0.1:9000");
 # ws.onmessage = function(e) {alert(e.data);};
 # ws.send('hello');
-from base_server.tcp_server.tcp_kernel import TCPKernel
 from kernel.chat_kernel import ChatKernel
 
 
-class MyServerProtocol(WebSocketServerProtocol, ChatKernel):
+class MyServerProtocol(WebSocketServerProtocol):
 
     def onConnect(self, request):
         print(f"Client connecting: {request.peer}")
-        self.sendMessage(bytes('Connect', encoding='utf-8'))
+        # self.sendMessage(bytes('Connect', encoding='utf-8'))
 
     def onClose(self, wasClean, code, reason):
         print(f"WebSocket connection closed: {reason}")
