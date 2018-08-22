24f13790687b9eb09946e70655a9d0763cf977da
Dmitriy <dimadeck94@gmail.com>
2018-08-22 14:40:01 +0300

    add kernel __init__ for constants

---
+++ kernel/__init__.py
@@ -0,0 +1,20 @@
+SERVER_SUFFIX = '[SERVER INFO]'
+SYSTEM_SUFFIX = '[SYSTEM INFO]'
+ERROR_SUFFIX = '[ERROR]'
+INFO_SUFFIX = '[INFO]'
+
+DELIMETER_MESSAGE = ' - '
+DELIMETER_CHAT = ': '
+
+MESSAGE_SERVER_START = 'server started on port:'
+MESSAGE_NEW_CONNECTION = 'New connection:'
+MESSAGE_LOGIN = 'login to chat.'
+MESSAGE_LOGOUT = 'logout from chat.'
+MESSAGE_FIRST_LOGIN = 'First login!'
+MESSAGE_ALREADY_LOGIN = 'Already login!'
+MESSAGE_USER_EXIST = 'Username already taken!'
+MESSAGE_NOT_FOUND = 'not found!'
+
+BASE_METHODS = ['login', 'msg', 'msgall', 'logout']
+SPECIAL_METHODS = ['whoami', 'userlist', 'debug']
+AVAILABLE_METHODS = BASE_METHODS + SPECIAL_METHODS
