74618ba400d561bd1a39589251cd9aa31679c31e
Dmitriy <dimadeck94@gmail.com>
2018-08-23 17:13:44 +0300

    add tw_ws to run

--- run.py
+++ run.py
@@ -5,10 +5,11 @@ import chat_tornado as tor_chat
 import chat_twisted as tw_chat
 import chat_ws_asyncio as aio_ws_chat
 import chat_ws_tornado as tor_ws_chat
+import twisted_websocket as tw_ws_chat
 from binder import main as bind
 
 LAUNCH = {'tw_chat': tw_chat.main, 'as_chat': as_chat.main, 'tor_chat': tor_chat.main,
-          'aio_ws_chat': aio_ws_chat.main, 'tor_ws_chat': tor_ws_chat.main,
+          'aio_ws_chat': aio_ws_chat.main, 'tor_ws_chat': tor_ws_chat.main, 'tw_ws_chat': tw_ws_chat.main,
           'bind': bind}
 BIND = ['as', 'tor', 'tcp_all', 'ws_all']
 LAUNCH_KEYS = list(LAUNCH.keys())
