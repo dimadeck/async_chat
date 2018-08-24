6ba8dd2582b71c6580992363c6f2746ba74ef6ec
Dmitriy <dimadeck94@gmail.com>
2018-08-24 16:14:52 +0300

    add render to twisted ws

--- chats/chat_ws_twisted.py
+++ chats/chat_ws_twisted.py
@@ -1,11 +1,25 @@
+import os
+
+from autobahn.twisted.resource import WebSocketResource
 from autobahn.twisted.websocket import WebSocketServerFactory
 from autobahn.twisted.websocket import WebSocketServerProtocol
-from autobahn.twisted.websocket import listenWS
 from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.server import Site
 
 from kernel.chat_kernel import ChatKernel
 
 VERSION = "Twisted_WS_Chat"
+CURRENT_DIR = os.path.dirname(os.path.abspath(__name__))
+TEMPLATES_DIR = os.path.join(CURRENT_DIR, 'chats', 'templates')
+index = os.path.join(TEMPLATES_DIR, 'index.html')
+
+
+class HttpResource(Resource):
+    def render_GET(self, request):
+        html = open(index, "r").read()
+        html = html.replace('{{version}}', VERSION)
+        return bytes(html, encoding='utf-8')
 
 
 class TwistedWsProtocol(WebSocketServerProtocol):
@@ -13,7 +27,6 @@ class TwistedWsProtocol(WebSocketServerProtocol):
         print('open')
 
     def onMessage(self, payload, isBinary):
-        print(payload)
         self.chat.engine(payload, self, 'None')
 
     def connectionLost(self, reason):
@@ -22,17 +35,24 @@ class TwistedWsProtocol(WebSocketServerProtocol):
 
 
 def main(connections=None, port=1234):
-    factory = WebSocketServerFactory(f'ws://127.0.0.1:{port}')
+    factory = WebSocketServerFactory(f'ws://127.0.0.1:{port}/ws')
     factory.protocol = TwistedWsProtocol
     factory.protocol.chat = ChatKernel(connections=connections, parse_strip='', method_send_message=send_message,
                                        method_close_connection=close_connection, version=VERSION, port=port)
-    listenWS(factory)
+
+    ws_resource = WebSocketResource(factory)
+
+    root = Resource()
+    root.putChild(b"", HttpResource())
+    root.putChild(b"ws", ws_resource)
+
+    site = Site(root)
+    reactor.listenTCP(port, site)
     reactor.run()
 
 
 def send_message(connection, message):
-    # mes = {'action': 'response', 'message': message}
-    connection.sendMessage(bytes(message, encoding='utf-8'))
+    connection.sendMessage(bytes(message, 'utf-8'))
 
 
 def close_connection(connection):
@@ -40,7 +60,4 @@ def close_connection(connection):
 
 
 if __name__ == '__main__':
-    # var ws = new WebSocket("ws://127.0.0.1:1234");
-    # ws.onmessage = function(e) {console.info(e.data);};
-    # ws.send('message');
     main()
