e988ca91829ac4dc498d5472d67158794368b7d1
Dmitriy <dimadeck94@gmail.com>
2018-08-29 15:24:28 +0300

    clear duplicate class

--- chats/AsyncIO.py
+++ chats/AsyncIO.py
@@ -1,62 +1,13 @@
 import asyncio
+from aiohttp import web
 
-import aiohttp_jinja2
-import jinja2
-from aiohttp import web, WSMsgType
-
-from chats import VERSION_AS, VERSION_AS_WS, get_setup_dict
-from kernel.fork_chat_kernel import ChatKernel
-from kernel.fork_connected import Connected
+from chats.chat_asyncio import AsyncioChat
+from chats.chat_ws_asyncio import AioChat
 
 
-class AioChat:
-    def __init__(self, connections, port):
-        setup_dict = get_setup_dict(connections, VERSION_AS_WS, port)
-        self.chat = ChatKernel(setup_dict)
-        self.app = web.Application()
-
-    def init_app(self):
-        aiohttp_jinja2.setup(self.app, loader=jinja2.PackageLoader('chats', 'templates'))
-        self.app.router.add_get('/', self.index)
-        self.app.router.add_get('/ws', self.ws)
-        return self.app
-
-    def index(self, request):
-        return aiohttp_jinja2.render_template('index.html', request, {'version': VERSION_AS_WS})
-
-    async def ws(self, request):
-        ws_current = web.WebSocketResponse()
-        await ws_current.prepare(request)
-        while True:
-            msg = await ws_current.receive()
-            if msg.type == WSMsgType.CLOSE:
-                await self.chat.logout_engine(ws_current)
-                break
-            else:
-                msg = bytes(msg.data, encoding='utf-8')
-                addr = request.transport.get_extra_info('peername')
-                if await self.chat.engine(msg, ws_current, addr) == -1:
-                    break
-        return ws_current
-
-
-class AsyncioChat:
-    def __init__(self, connections, port):
-        setup_dict = get_setup_dict(connections, VERSION_AS, port)
-        self.chat = ChatKernel(setup_dict)
-
-    async def handle_client(self, reader, writer):
-        while True:
-            request = (await reader.read(1024))
-            addr = writer.get_extra_info('peername')
-            if await self.chat.engine(request, writer, addr) == -1:
-                break
-
-
-def main(port1=8000, port2=8080):
-    connections = Connected()
+def main(port1=8000, port2=8080, connections=None):
     server = AsyncioChat(connections=connections, port=port1)
-    server1 = AioChat(connections=connections, port=port2)
+    server1 = AioChat(connections=server.chat.connections, port=port2)
     app = server1.init_app()
 
     server.chat.set_outside_request(server1.chat.from_outside)
