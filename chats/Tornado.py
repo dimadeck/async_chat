17aeeb86f8865bdc348d375de37c99244c2a7f42
Dmitriy <dimadeck94@gmail.com>
2018-09-04 16:00:29 +0300

    adaptive both tornado servers for new CH
    Signed-off-by: Dmitriy <dimadeck94@gmail.com>

--- chats/Tornado.py
+++ chats/Tornado.py
@@ -1,18 +1,21 @@
 import tornado
 
+from chats import TorServer, TorWsServer
 from chats.chat_tornado import EchoServer
 from chats.chat_ws_tornado import Application
+from kernel.chat_kernel import ChatKernel
+from kernel.sender import Sender
 
 
-def main(port1=8000, port2=8080, connections=None):
-    server1 = EchoServer(connections=connections, port=port1)
+def main(port1=8000, port2=8080):
+    sender = Sender()
+    chat1 = ChatKernel(TorServer, port1, sender)
+    server1 = EchoServer(chat1)
     server1.listen(port1)
 
-    server2 = Application(server1.chat.connections, port2)
+    chat2 = ChatKernel(TorWsServer, port2, chat1.sender)
+    server2 = Application(chat2)
     server2.listen(port2)
-
-    server1.chat.set_outside_request(server2.chat.from_outside)
-    server2.chat.set_outside_request(server1.chat.from_outside)
 
     try:
         tornado.ioloop.IOLoop.instance().start()
