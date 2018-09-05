37b387ba4d1934ce2571ec1b5d5d2394685f85c4
Dmitriy <dimadeck94@gmail.com>
2018-09-05 13:52:54 +0300

    add some tests for connected: add_version, add_connection, get_connection_by_version, get_version_by_connection
    Signed-off-by: Dmitriy <dimadeck94@gmail.com>

--- kernel/tests/test_connected.py
+++ kernel/tests/test_connected.py
@@ -1,15 +1,73 @@
 import unittest
 
+from kernel.connected import Connected
+
 
 class ConnectedTest(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
-        cls.expected_line1 = 1
+    def setUp(cls):
+        cls.connections = Connected()
 
-    def test_1(self):
-        line2 = 1
-        self.assertEqual(self.expected_line1, line2)
+    def test_connected_add_version_ok(self):
+        expected_state = 0
+        state = self.connections.add_version_header('test_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_add_version_bad(self):
+        expected_state = -1
+        self.connections.add_version_header('test_version')
+        state = self.connections.add_version_header('test_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_add_connection_ok(self):
+        expected_state = 0
+        self.connections.add_version_header('test_version')
+        state = self.connections.add_connection('test_connection_1', 'test_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_add_connection_bad_already_in_list(self):
+        expected_state = -1
+        self.connections.add_version_header('test_version')
+        self.connections.add_connection('test_connection_1', 'test_version')
+        state = self.connections.add_connection('test_connection_1', 'test_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_add_connection_bad_version(self):
+        expected_state = -2
+        self.connections.add_version_header('test_version')
+        state = self.connections.add_connection('test_connection_1', 'test1_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_get_connections_by_version(self):
+        expected_state = ['test_connection_1', 'test_connection_2']
+        self.connections.add_version_header('test_version')
+        self.connections.add_connection('test_connection_1', 'test_version')
+        self.connections.add_connection('test_connection_2', 'test_version')
+        state = self.connections.get_connections_by_version('test_version')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_get_version_by_connections_ok(self):
+        expected_state = 'test_version1'
+        self.connections.add_version_header('test_version')
+        self.connections.add_version_header('test_version1')
+        self.connections.add_connection('test_connection_1', 'test_version')
+        self.connections.add_connection('test_connection_2', 'test_version1')
+        state = self.connections.get_version_by_connection('test_connection_2')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_get_version_by_connections_bad_connection_not_exist(self):
+        expected_state = -2
+        self.connections.add_version_header('test_version')
+        self.connections.add_connection('test_connection_1', 'test_version')
+        state = self.connections.get_version_by_connection('test_connection_2')
+        self.assertEqual(expected_state, state)
+
+    def test_connected_get_version_by_connections_bad_version_not_registered(self):
+        expected_state = -1
+        self.connections.add_connection('test_connection_1', 'test_version')
+        state = self.connections.get_version_by_connection('test_connection_1')
+        self.assertEqual(expected_state, state)
 
 
 if __name__ == '__main__':
