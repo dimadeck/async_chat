9b8a173e1ce302dc6fb4fc5a08a5fa9f09e869eb
Dmitriy <dimadeck94@gmail.com>
2018-09-06 12:14:06 +0300

    adaptive tornado_ws for new html-architecture
    Signed-off-by: Dmitriy <dimadeck94@gmail.com>

--- chats/chat_ws_tornado.py
+++ chats/chat_ws_tornado.py
@@ -7,11 +7,6 @@ from kernel.chat_kernel import ChatKernel
 from kernel.sender import Sender
 
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('templates/index.html', version=TorWsServer.VERSION)
-
-
 class WebSocket(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         message = bytes(message, encoding='utf-8')
@@ -26,8 +21,8 @@ class Application(tornado.web.Application):
     def __init__(self, chat):
         self.chat = chat
         handlers = (
-            (r'/', MainHandler),
-            (r'/ws', WebSocket))
+            (r'/ws', WebSocket),
+            (r'/(.*)', tornado.web.StaticFileHandler, {"path": "chats/templates/", 'default_filename': "index.html"}))
 
         tornado.web.Application.__init__(self, handlers)
 
